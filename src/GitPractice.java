7eba9decda1bf498dbc2e8a9fff585460cdaf850
Arman Hussaini <arman.hussaini@outlook.com>
2022-01-05 23:53:04 -0500

    First Commit

---
+++ src/GitPractice.java
@@ -0,0 +1,12 @@
+
+public class GitPractice {
+	 public static void main (String [] args) {
+
+		 String motivated = "yes";
+		 if (motivated == "yes") {
+			 System.out.println("Keep Going!");
+		 } else {
+			 System.out.println("Check out the path!");
+		 }
+}
+}
